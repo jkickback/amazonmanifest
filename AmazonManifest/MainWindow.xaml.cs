3f532c44e31e52fc99d6c1917977a032aa31bcdd
jkickback <jkickback@gmail.com>
2015-04-29 22:59:10 -0400

    Added loading of spreadsheet into gridview object. Ready to start on searching and highlighting items while storing them for a final report.

--- AmazonManifest/MainWindow.xaml.cs
+++ AmazonManifest/MainWindow.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AmazonManifest.DataTypes;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace AmazonManifest
 {
@@ -24,29 +27,36 @@ namespace AmazonManifest
     public partial class MainWindow : Window
     {
         private string _xlsFileName;
+        public DataSet SpreadsheetData; //Original Spreedsheet in DS form
+        public ObservableCollection<SpreadSheetRow> SpreadsheetResults;  //View with certain columns that user sees
+        public string ScannedText;
 
         public MainWindow()
         {
             InitializeComponent();
+            SpreadsheetResults = new ObservableCollection<SpreadSheetRow>();
+            SpreadsheetResults.CollectionChanged += SpreadsheetResults_CollectionChanged;
+
+        }
+
+        public void SpreadsheetResults_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            SpreadsheetView.ItemsSource = SpreadsheetResults;
         }
 
 
-
+        #region File Handlers
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
             // Create OpenFileDialog 
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
 
-
-
             // Set filter for file extension and default file extension 
             dlg.DefaultExt = ".xlsx";
             dlg.Filter = "XLSX Files (*.xlsx)|*.xlsx";
 
-
             // Display OpenFileDialog by calling ShowDialog method 
             Nullable<bool> result = dlg.ShowDialog();
-
 
             // Get the selected file name and display in a TextBox 
             if (result == true)
@@ -55,34 +65,43 @@ namespace AmazonManifest
                 _xlsFileName = dlg.FileName;
             }
 
-            _readSourceFile();
+            _loadSpreadsheet();
 
         }
 
-        private void _readSourceFile()
+        private void _loadSpreadsheet()
         {
 
-            FileStream stream = File.Open(_xlsFileName, FileMode.Open, FileAccess.Read);
-
-            //...
-            //2. Reading from a OpenXml Excel file (2007 format; *.xlsx)
-            IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
-            //...
-            //3. DataSet - The result of each spreadsheet will be created in the result.Tables
-            //DataSet result = excelReader.AsDataSet();
-            //...
-            //4. DataSet - Create column names from first row
-            excelReader.IsFirstRowAsColumnNames = true;
-            DataSet result = excelReader.AsDataSet();
-
-            //5. Data Reader methods
-            while (excelReader.Read())
-            {
-                //excelReader.GetInt32(0);
+            using (FileStream stream = File.Open(_xlsFileName, FileMode.Open, FileAccess.Read))
+            { 
+                using (IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream))
+                {
+                    excelReader.IsFirstRowAsColumnNames = true;
+                    DataSet result = excelReader.AsDataSet();
+                    SpreadsheetData = result;
+                }
             }
 
-            //6. Free resources (IExcelDataReader is IDisposable)
-            excelReader.Close();
+            //SpreadsheetView.ItemsSource = SpreadsheetData.Tables[Properties.Settings.Default.SheetName].DefaultView;
+            DataTable table = SpreadsheetData.Tables[Properties.Settings.Default.SheetName];
+            int index = 1;
+            foreach (DataRow row in table.Rows)
+            {
+                SpreadSheetRow newRow = new SpreadSheetRow(row, index);
+                SpreadsheetResults.Add(newRow);
+                index++;
+            }
+
+
+        }
+        #endregion
+
+
+        private void SearchDataset()
+        {
+            //Search through columns U (ASIN), V (UPC), W (EAN), AP (LPN), X (FCSKU) and  AP (ANSKU).
+
+
         }
 
     }
